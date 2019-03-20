c662bc8f9fca0a77dd67fa53aec2f114f7784dac
Jenkins CI <sdk@pushwoosh.com>
2019-03-20 15:51:20 +0700

    Pushwoosh Trigger.io Plugin 3.4.0
    https://github.com/Pushwoosh/pushwoosh-trigger-io/releases/tag/3.4.0

---
+++ Geozones/inspector/an-inspector/ForgeModule/build/generated/source/buildConfig/vanilla/release/io/trigger/forge/android/modules/pushwoosh_geozones/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.trigger.forge.android.modules.pushwoosh_geozones;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "io.trigger.forge.android.modules.pushwoosh_geozones";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "vanilla";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
